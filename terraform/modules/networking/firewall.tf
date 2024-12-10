2a128aa28ea943f43162abd5219cd5047674f8f4
Travis Woodward <Travis.Woodward@softwire.com>
2024-12-10 14:03:10 +0000

    PRSD-573: Switches to multiple AZs

--- terraform/modules/networking/firewall.tf
+++ terraform/modules/networking/firewall.tf
@@ -46,8 +46,13 @@ resource "aws_networkfirewall_firewall" "main" {
   name                = "network-firewall-${var.environment_name}"
   firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
   vpc_id              = aws_vpc.main.id
-  subnet_mapping {
-    subnet_id = aws_subnet.firewall.id
+
+  dynamic "subnet_mapping" {
+    for_each = aws_subnet.firewall
+
+    content {
+      subnet_id = subnet_mapping.value.id
+    }
   }
 }
 
