69f4aac6b14c503616aadc10a23a79229f10ddd3
Travis Woodward <Travis.Woodward@softwire.com>
2025-01-24 14:36:57 +0000

    PRSD-790: Fixes broken iam policy document

--- terraform/modules/secrets/iam.tf
+++ terraform/modules/secrets/iam.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "kms_secrets_decrypt" {
   statement {
     principals {
@@ -6,6 +8,17 @@ data "aws_iam_policy_document" "kms_secrets_decrypt" {
     }
 
     actions = ["kms:Decrypt"]
+
+    resources = [aws_kms_key.prsdb_webapp_secrets.arn]
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = ["kms:*"]
 
     resources = [aws_kms_key.prsdb_webapp_secrets.arn]
   }
