ef3d981494d600cfdba1d80a227cfecfb282e322
Travis Woodward <Travis.Woodward@softwire.com>
2025-02-18 12:03:57 +0000

    PRSD-751: Remove unused variable

--- terraform/integration/variables.tf
+++ terraform/integration/variables.tf
@@ -3,9 +3,3 @@ variable "ssl_certs_created" {
   type        = bool
   default     = true
 }
-
-variable "task_definition_created" {
-  description = "Indicates whether the initial task definition has been created"
-  type        = bool
-  default     = true
-}
