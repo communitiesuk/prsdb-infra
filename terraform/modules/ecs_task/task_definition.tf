5c91b48f78a1950364a7176a9cf66d2ba3379777
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2025-02-18 08:01:32 +0000

    PRSD-751: Adds logging and changes user in ECS task (#34)

--- terraform/modules/ecs_task/task_definition.tf
+++ terraform/modules/ecs_task/task_definition.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "main" {
       name      = var.task_name
       essential = true
       image     = var.container_image
-      user      = "nonroot"
+      user      = "root"
 
       portMappings = [
         {
@@ -21,6 +21,17 @@ resource "aws_ecs_task_definition" "main" {
           hostPort      = var.container_port
         }
       ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.webapp_log_group.id
+          awslogs-region        = "eu-west-2"
+          awslogs-stream-prefix = var.environment_name
+          mode                  = "non-blocking"
+          max-buffer-size       = "4m" # See this analysis of how to choose a buffer size in non-blocking mode: https://github.com/moby/moby/issues/45999.
+        }
+      }
 
       environment = var.environment_variables
       secrets     = var.secrets
