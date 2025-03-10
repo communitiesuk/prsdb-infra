33ef43db4c6d17ae5a3c90e9fa52168b04dea717
Alex Read <alexander.read@communities.gov.uk>
2025-03-10 16:07:14 +0000

    PRSD-898: Add recommended security settings

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -76,4 +76,11 @@ resource "aws_instance" "bastion" {
   subnet_id              = var.bastion_subnet_ids[count.index]
   vpc_security_group_ids = [aws_security_group.bastion.id]
   iam_instance_profile   = aws_iam_instance_profile.ssm_bastion.name
+
+  root_block_device {
+    encrypted = true
+  }
+  metadata_options {
+    http_tokens = "required"
+  }
 }
