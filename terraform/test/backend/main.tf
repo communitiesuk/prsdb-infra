8b7c9059d2100df147f44277ec41887bfac4458f
Alex Read <alexander.read@communities.gov.uk>
2025-03-14 14:01:18 +0000

    PRSD-966: Create preliminary terraform files for test

---
+++ terraform/test/backend/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~>1.9.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+
+   backend "s3" {
+     bucket         = "prsdb-tfstate-test"
+     dynamodb_table = "tfstate-lock-test"
+     encrypt        = true
+     key            = "prsdb-infra-test-tfstate"
+     region         = "eu-west-2"
+   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "terraform-backend" {
+  source           = "../../modules/terraform_backend"
+  environment_name = "test"
+}
