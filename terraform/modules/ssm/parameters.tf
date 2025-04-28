c0ba1849195b5f784ce89607f27696c84f97126c
Alex Read <alexander.read@softwire.com>
2025-04-28 13:57:05 +0100

    PRSD-1065: Add environment variables (#77)

--- terraform/modules/ssm/parameters.tf
+++ terraform/modules/ssm/parameters.tf
@@ -29,3 +29,33 @@ resource "aws_ssm_parameter" "one_login_issuer_url" {
     ignore_changes = [value]
   }
 }
+
+resource "aws_ssm_parameter" "epc_register_client_id" {
+  name  = "${var.environment_name}-prsdb-epc-client-id"
+  type  = "String"
+  value = "default_to_be_set_manually" # To be set manually on AWS
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "epc_register_token_uri" {
+  name  = "${var.environment_name}-prsdb-epc-token-uri"
+  type  = "String"
+  value = "default_to_be_set_manually" # To be set manually on AWS
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "epc_register_base_url" {
+  name  = "${var.environment_name}-prsdb-epc-register-base-url"
+  type  = "String"
+  value = "default_to_be_set_manually" # To be set manually on AWS
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
