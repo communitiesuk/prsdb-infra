a4bf0f31bebdbb4d09cbff38fe01c24fd3e72df3
Alex Read <alexander.read@softwire.com>
2025-06-20 10:00:45 +0100

    PRSD-1282: Add example ecs task to virus scan events (#98)

---
+++ terraform/modules/ecs_service/outputs.tf
@@ -0,0 +1,9 @@
+output "ecs_cluster_arn" {
+  value       = aws_ecs_cluster.main.arn
+  description = "The arn of the ecs cluster for this environment"
+}
+
+output "ecs_security_group_ids" {
+  value       = [aws_security_group.ecs.id]
+  description = "List of security group IDs attached to the ECS tasks"
+}
