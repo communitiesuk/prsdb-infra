e3d869195d03da9125622fb247e54b3094c59f9c
Alex Read <alexander.read@softwire.com>
2025-06-25 12:19:24 +0100

    PRSD-1282: Use correct service principal (#110)

--- terraform/modules/file_upload/kinesis_target.tf
+++ terraform/modules/file_upload/kinesis_target.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "kinesis_firehose_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["kinesis.amazonaws.com"]
+      identifiers = ["firehose.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
