450e5dfe949b359d8d3298820a70504d09261095
Jasmin Conterio <Jasmin.Conterio@softwire.com>
2025-07-03 16:47:10 +0100

    PRSD-1294: Add id-verification as an exception (#119)

--- terraform/modules/frontdoor/url_rewriter.js
+++ terraform/modules/frontdoor/url_rewriter.js
@@ -1,5 +1,5 @@
 function handler(event) {
-    const exceptions = ["signout","confirm-sign-out", "error", "assets"];
+    const exceptions = ["signout","confirm-sign-out", "error", "assets", "id-verification", "login", "logout"];
     const request = event.request;
     const hostName = request.headers.host.value;
     let pathSegments = request.uri.split('/');
