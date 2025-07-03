005396ffa254380b87a028885119d9f24e83cdc6
JasCon <jasmin.conterio@softwire.com>
2025-07-03 09:47:53 +0100

    PRSD-1294: get the test script working

--- test/js/url_rewriter.test.js
+++ test/js/url_rewriter.test.js
@@ -1,21 +1,20 @@
 const rewire = require('rewire');
-const test = require("node:test");
-const assert = require("node:assert");
+const url_rewriter = rewire('../../terraform/modules/frontdoor/url_rewriter.js').__get__('url_rewriter');
 
-const sut = rewire('../../terraform/modules/frontdoor/url_rewriter.js').__get__('url_rewriter');
+describe('url_rewriter', () => {
+    it('inserts /landlord into a URL for the register-home-to-rent domain', () => {
+        // given
+        const event = {
+            request: {
+                method: 'GET',
+                uri: 'https://integration.register-home-to-rent.test.communities.gov.uk/dashboard'
+            }
+        };
 
-test('url_rewriter inserts /landlord into a URL for the register-home-to-rent domain', () => {
-    // given
-    const event = {
-        request: {
-            method: 'GET',
-            uri: 'https://integration.register-home-to-rent.test.communities.gov.uk/dashboard'
-        }
-    };
-
-    // when
-    const new_uri = sut(event).uri;
+        // when
+        const new_uri = url_rewriter(event).uri;
 
         // then
-        assert.equal(new_uri,'https://integration.register-home-to-rent.test.communities.gov.uk/landlord/dashboard');
-})
+        expect(new_uri).toBe('https://integration.register-home-to-rent.test.communities.gov.uk/landlord/dashboard');
+    });
+});
