ed78e636e9db06c5135561441b20e60fb9793307
Alex Read <alexander.read@softwire.com>
2025-07-16 09:31:05 +0100

    PRSD-1284: Adds base urls to services (#125)

--- terraform/modules/ssm/parameters.tf
+++ terraform/modules/ssm/parameters.tf
@@ -69,3 +69,15 @@ resource "aws_ssm_parameter" "epc_certificate_base_url" {
     ignore_changes = [value]
   }
 }
+
+resource "aws_ssm_parameter" "landlord_base_url" {
+  name  = "${var.environment_name}-prsdb-landlord-base-url"
+  type  = "String"
+  value = var.landlord_base_url
+}
+
+resource "aws_ssm_parameter" "local_authority_base_url" {
+  name  = "${var.environment_name}-prsdb-local-authority-base-url"
+  type  = "String"
+  value = var.local_authority_base_url
+}
