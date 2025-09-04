c5ee3f336c3c0ff6a5bf4d2bfa8ea70bd15890df
JasCon <jasmin.conterio@softwire.com>
2025-09-04 09:57:31 +0100

    PRSD-981: terraform fmt

--- terraform/modules/bastion/ssm_patch.tf
+++ terraform/modules/bastion/ssm_patch.tf
@@ -1,15 +1,15 @@
 resource "aws_ssm_maintenance_window" "bastion_patch" {
-  name        = "${var.environment_name}-ssm-patch-window"
-  schedule    = "cron(0 2 ? * WED *)" # Every Wednesday at 2 AM UTC
-  duration    = 3
-  cutoff      = 1
+  name     = "${var.environment_name}-ssm-patch-window"
+  schedule = "cron(0 2 ? * WED *)" # Every Wednesday at 2 AM UTC
+  duration = 3
+  cutoff   = 1
 }
 
 resource "aws_ssm_maintenance_window_task" "bastion_patch" {
-  window_id        = aws_ssm_maintenance_window.bastion_patch.id
-  task_type        = "RUN_COMMAND"
-  task_arn         = "AWS-RunPatchBaseline"
-  priority         = 1
+  window_id = aws_ssm_maintenance_window.bastion_patch.id
+  task_type = "RUN_COMMAND"
+  task_arn  = "AWS-RunPatchBaseline"
+  priority  = 1
 
   targets {
     key    = "InstanceIds"
@@ -18,7 +18,7 @@ resource "aws_ssm_maintenance_window_task" "bastion_patch" {
 
   task_invocation_parameters {
     run_command_parameters {
-      comment          = "Default Baseline Install"
+      comment = "Default Baseline Install"
       cloudwatch_config {
         cloudwatch_log_group_name = aws_cloudwatch_log_group.bastion_log_group.id
         cloudwatch_output_enabled = true
