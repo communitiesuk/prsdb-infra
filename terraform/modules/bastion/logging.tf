b1827d0df754d150c594c980288f07572cf5c278
JasCon <jasmin.conterio@softwire.com>
2025-09-04 10:13:25 +0100

    PRSD-981: Add tfsec:ignore

--- terraform/modules/bastion/logging.tf
+++ terraform/modules/bastion/logging.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "bastion_log_group" {
   name              = "${var.environment_name}-bastion"
   retention_in_days = 60
