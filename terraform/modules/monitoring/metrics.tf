86ec45460a932872b7bd7788fcdd736fc7ef5ea2
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-09-16 10:49:12 +0100

    PRSD-1526: Adds WizAccess-Role exemption to unauthorized_api_calls alarm

--- terraform/modules/monitoring/metrics.tf
+++ terraform/modules/monitoring/metrics.tf
@@ -16,8 +16,9 @@ resource "aws_cloudwatch_log_metric_filter" "unauthorized_api_calls" {
   log_group_name = module.cloudtrail_cloudwatch_group.name
   name           = "unauthorized-api-calls-${var.environment_name}"
   pattern        = <<EOT
-    {($.errorCode = UnauthorizedOperation) ||
-     ($.errorCode = AccessDenied)}
+    {(($.errorCode = UnauthorizedOperation) ||
+      ($.errorCode = AccessDenied)) &&
+     ($.userIdentity.sessionContext.sessionIssuer.userName != "WizAccess-Role")}
   EOT
 
   metric_transformation {
