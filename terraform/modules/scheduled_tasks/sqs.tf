833f704ea70065d8e6e9e71d984006b4fecb8386
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2025-11-03 16:14:32 +0000

    PRSD-1604: Fixes linting error and reduce frequence of example scheduled task

--- terraform/modules/scheduled_tasks/sqs.tf
+++ terraform/modules/scheduled_tasks/sqs.tf
@@ -3,5 +3,5 @@
 resource "aws_sqs_queue" "scheduled_tasks_dead_letter_queue" {
   name = "${var.environment_name}-prsdb-scheduled-tasks-dead-letter-queue"
 
-  message_retention_seconds = 1209600 // 14 days, the maximum
+  message_retention_seconds = 1209600 # 14 days, the maximum
 }
