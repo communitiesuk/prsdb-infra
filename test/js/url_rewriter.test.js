13c6a2e562cfc0640dfbdf3232ccfb0013cb7f85
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2026-03-30 11:45:10 +0100

    PDJB-607: Exclude /login from URL rewriting to fix OAuth callback routing

--- test/js/url_rewriter.test.js
+++ test/js/url_rewriter.test.js
@@ -178,7 +178,18 @@ describe('url_rewriter', () => {
 
             // then
             expect(new_event.headers.host.value + new_event.uri).toBe('https://register-home-to-rent.communities.gov.uk/.well-known/security.txt');
-        })
+        });
+
+        it('returns the original url for the /login endpoint', () => {
+            // given
+            const event = createRequestEvent(registerHomeToRentHost, '/login/oauth2/code/one-login');
+
+            // when
+            const new_event = url_rewriter(event);
+
+            // then
+            expect(new_event.headers.host.value + new_event.uri).toBe('https://register-home-to-rent.communities.gov.uk/login/oauth2/code/one-login');
+        });
     });
 
     describe('for the search-landlord-home-information domain', () => {
@@ -368,6 +379,17 @@ describe('url_rewriter', () => {
 
             // then
             expect(new_event.headers.host.value + new_event.uri).toBe('https://search-landlord-home-information.communities.gov.uk/system-operator');
+        });
+
+        it('returns the original url for the /login endpoint', () => {
+            // given
+            const event = createRequestEvent(searchLandlordHomeInformationHost, '/login/oauth2/code/one-login');
+
+            // when
+            const new_event = url_rewriter(event);
+
+            // then
+            expect(new_event.headers.host.value + new_event.uri).toBe('https://search-landlord-home-information.communities.gov.uk/login/oauth2/code/one-login');
         });
     });
 });
