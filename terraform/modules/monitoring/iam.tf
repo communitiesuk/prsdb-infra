ecd3b4d419f1d0a8580ae7c0a73a0a96d447795f
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2026-05-29 08:15:01 +0100

    PDJB-190: Rate-limit probing traffic and split alarm SNS topics by severity (#280)

--- terraform/modules/monitoring/iam.tf
+++ terraform/modules/monitoring/iam.tf
@@ -46,19 +46,52 @@ resource "aws_iam_role_policy_attachment" "cloudtrail_cloudwatch_policy_attachme
   policy_arn = aws_iam_policy.cloudtrail_cloudwatch_policy.arn
 }
 
-resource "aws_sns_topic_policy" "cloudwatch_to_alarm_sns" {
-  arn    = aws_sns_topic.alarm_sns_topic.arn
-  policy = data.aws_iam_policy_document.cloudwatch_to_sns_policy_document.json
+resource "aws_sns_topic_policy" "cloudwatch_to_critical_alarm_sns" {
+  arn    = aws_sns_topic.critical_alarm_sns_topic.arn
+  policy = data.aws_iam_policy_document.cloudwatch_to_critical_sns_policy_document.json
 }
 
-data "aws_iam_policy_document" "cloudwatch_to_sns_policy_document" {
+resource "aws_sns_topic_policy" "cloudwatch_to_non_critical_alarm_sns" {
+  arn    = aws_sns_topic.non_critical_alarm_sns_topic.arn
+  policy = data.aws_iam_policy_document.cloudwatch_to_non_critical_sns_policy_document.json
+}
+
+moved {
+  from = aws_sns_topic_policy.cloudwatch_to_alarm_sns
+  to   = aws_sns_topic_policy.cloudwatch_to_critical_alarm_sns
+}
+
+data "aws_iam_policy_document" "cloudwatch_to_critical_sns_policy_document" {
   statement {
     effect = "Allow"
     actions = [
       "sns:Publish"
     ]
     resources = [
-      aws_sns_topic.alarm_sns_topic.arn
+      aws_sns_topic.critical_alarm_sns_topic.arn
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "cloudwatch.amazonaws.com"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch_to_non_critical_sns_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sns:Publish"
+    ]
+    resources = [
+      aws_sns_topic.non_critical_alarm_sns_topic.arn
     ]
     principals {
       type = "Service"
--- terraform/modules/monitoring/sns.tf
+++ terraform/modules/monitoring/sns.tf
@@ -1,27 +1,35 @@
 # non-sensitive
 # tfsec:ignore:aws-sns-enable-topic-encryption
-resource "aws_sns_topic" "alarm_sns_topic" {
-  name         = "${var.environment_name}-alarm-sns-topic"
-  display_name = "Notifications for cloudwatch alarms in ${var.environment_name} environment"
+resource "aws_sns_topic" "critical_alarm_sns_topic" {
+  name         = "${var.environment_name}-critical-alarm-sns-topic"
+  display_name = "Notifications for critical cloudwatch alarms in ${var.environment_name} environment"
 }
 
-resource "aws_sns_topic_subscription" "alarm_email_subscription" {
-  topic_arn = aws_sns_topic.alarm_sns_topic.arn
+resource "aws_sns_topic_subscription" "critical_alarm_email_subscription" {
+  topic_arn = aws_sns_topic.critical_alarm_sns_topic.arn
   protocol  = "email"
-  endpoint  = var.alarm_email_address
+  endpoint  = var.critical_alarm_email_address
 }
 
 # non-sensitive
 # tfsec:ignore:aws-sns-enable-topic-encryption
-resource "aws_sns_topic" "us_alarm_sns_topic" {
-  name         = "${var.environment_name}-us-alarm-sns-topic"
-  display_name = "Notifications for cloudwatch alarms in ${var.environment_name} environment and us-east-1 region"
-  provider     = aws.us-east-1
+resource "aws_sns_topic" "non_critical_alarm_sns_topic" {
+  name         = "${var.environment_name}-non-critical-alarm-sns-topic"
+  display_name = "Notifications for non-critical cloudwatch alarms in ${var.environment_name} environment"
 }
 
-resource "aws_sns_topic_subscription" "us_alarm_email_subscription" {
-  topic_arn = aws_sns_topic.us_alarm_sns_topic.arn
+resource "aws_sns_topic_subscription" "non_critical_alarm_email_subscription" {
+  topic_arn = aws_sns_topic.non_critical_alarm_sns_topic.arn
   protocol  = "email"
-  endpoint  = var.alarm_email_address
-  provider  = aws.us-east-1
+  endpoint  = var.non_critical_alarm_email_address
+}
+
+moved {
+  from = aws_sns_topic.alarm_sns_topic
+  to   = aws_sns_topic.critical_alarm_sns_topic
+}
+
+moved {
+  from = aws_sns_topic_subscription.alarm_email_subscription
+  to   = aws_sns_topic_subscription.critical_alarm_email_subscription
 }
